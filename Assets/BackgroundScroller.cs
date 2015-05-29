92cff7a331f040c53990e1cbc80bd3cfc76b91a7
mofr <mofr@zond.org>
2015-05-29 15:05:46 +0600

    Add infinite scrolling background

---
+++ Assets/BackgroundScroller.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	public Sprite firstSprite;
+	public Sprite[] loopingSprites;
+
+	public float speed = 1.0f;
+
+	GameObject bg1;
+	GameObject bg2;
+
+	SpriteRenderer renderer1;
+	SpriteRenderer renderer2;
+	
+	void Start () {
+		GameObject scrollingBackground = new GameObject ("Scrolling Background");
+
+		bg1 = new GameObject ("bg1");
+		bg1.transform.parent = scrollingBackground.transform;
+		renderer1 = bg1.AddComponent<SpriteRenderer>();
+		renderer1.sprite = firstSprite;
+
+		bg2 = new GameObject ("bg2");
+		bg2.transform.parent = scrollingBackground.transform;
+		renderer2 = bg2.AddComponent<SpriteRenderer>();
+		renderer2.sprite = randomSprite();
+
+		bg2.transform.Translate (renderer1.sprite.bounds.size.x/2 + renderer2.sprite.bounds.size.x/2, 0, 0);
+	}
+
+	void Update () {
+		float offset = Time.deltaTime * -speed;
+
+		bg1.transform.Translate (offset, 0, 0);
+		bg2.transform.Translate (offset, 0, 0);
+
+		if (bg1.transform.position.x <= -renderer1.sprite.bounds.size.x) {
+			Swap();
+		}
+	}
+
+	Sprite randomSprite(Sprite permitted = null) {
+		Sprite sprite;
+		do {
+			sprite = loopingSprites[Random.Range (0, loopingSprites.Length)];
+		} while(sprite == permitted && loopingSprites.Length > 1);
+		return sprite;
+	}
+
+	void Swap() {
+		renderer1.sprite = randomSprite(renderer2.sprite);
+		bg1.transform.position = bg2.transform.position + new Vector3 (renderer2.bounds.size.x/2 + renderer1.bounds.size.x/2, 0, 0);
+
+		GameObject tmpObject = bg1;
+		bg1 = bg2;
+		bg2 = tmpObject;
+
+		SpriteRenderer tmpRenderer = renderer1;
+		renderer1 = renderer2;
+		renderer2 = tmpRenderer;
+	}
+}
