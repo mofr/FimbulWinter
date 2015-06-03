05b391ba749cf9fe283f084392edb4cd4e75841e
mofr <mofr@zond.org>
2015-06-03 11:37:14 +0600

    Добавил пар изо рта ярлу

---
+++ Assets/Breath/Breath.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breath : MonoBehaviour {
+
+	public GameObject particleSystem;
+	public GameObject attachTo;
+
+	public void OnBreathOut() {
+		GameObject ps = Instantiate (particleSystem);
+		if (attachTo) {
+			ps.transform.SetParent(attachTo.transform, false);
+		}
+		Destroy (ps, 3);
+	}
+}
